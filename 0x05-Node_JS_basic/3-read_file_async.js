81cf03cff75f9e1df2fef5f8bdd3572c3bc32d2d
truth <trutherkadi@gmail.com>
2024-11-27 18:16:14 +0000

    js

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -20,7 +20,7 @@ const studentPropNames = dbFieldNames.slice(0, dbFieldNames.length - 1);
 for (const line of fileLines.slice(1)){
 const studentRecord = line.split(',');
 const studentPropValues = studentRecord.slice(0, studentRecord.length - 1);
-const field = studentRecord.slice(0, studentRecord.length - 1);
+const field = studentRecord[0, studentRecord.length - 1];
 if (!Object.keys(studentGroups).includes(field)){
 studentGroups[field] = [];}
 const studentEntries = studentPropNames.map((propName, idx) => [propName, studentPropValues[idx]]);
