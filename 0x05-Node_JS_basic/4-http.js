69c22d6b662cd046efffebe2904bb1cff6e91d4a
truth <trutherkadi@gmail.com>
2024-11-27 18:42:10 +0000

    server js

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -3,7 +3,7 @@ const PORT = 1245;
 const HOST = 'localhost';
 
 const app = http.createServer();
-app.on(reques, (_, res) => {
+app.on('request', (_, res) => {
 const responseText = 'Hello Holberton School!';
 res.setHeader('Content-Type', 'text/plain');
 res.setHeader('Content-Length', responseText.length);
@@ -12,6 +12,6 @@ res.write(Buffer.from(responseText));
 });
 
 app.listen(PORT, HOST, () => {
-process.stdout.write('Server listening at -> http://${HOST}:${PORT}');});
+process.stdout.write(`Server listening at -> http://${HOST}:${PORT}`);});
 
 module.exports = app;
