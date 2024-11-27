d178a9f82907cb9b34d0b1e03cb9a554e24bc7d1
truth <trutherkadi@gmail.com>
2024-11-27 21:04:01 +0000

    js

--- 0x05-Node_JS_basic/6-http_express.js
+++ 0x05-Node_JS_basic/6-http_express.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 1245;
 app.get('/', (_, res) => {
-res.send('Hello Holberton School');
+res.send('Hello Holberton School!');
 });
 app.listen(PORT, () => {
 console.log(`Server listening on PORT ${PORT}`);
