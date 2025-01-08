2a9e933740a57c36cc5796ba08c8877d35bb6f8f
truther <trutherkadi@gmail.com>
2025-01-08 18:37:14 +0000

    unittest

--- 0x06-unittests_in_js/7-skip.test.js
+++ 0x06-unittests_in_js/7-skip.test.js
@@ -9,8 +9,8 @@ describe('Testing numbers', () => {
     expect(2 === 2).to.be.true;
   });
 
-  it('1 is equal to 3', function() {
-    expect(1).to.equal(3);  // This will force the test to be counted but fail
+  it.skip('1 is equal to 3', () => {
+    expect(1 === 3).to.be.true;
   });
 
   it('3 is equal to 3', () => {
